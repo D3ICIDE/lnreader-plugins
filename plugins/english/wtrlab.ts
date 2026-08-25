a4228c0b8bf3487f27c5e2f1974f3c4229e3fa9d
D3ICIDE <183315953+D3ICIDE@users.noreply.github.com>
2026-08-25 18:18:25 +0530

    replaced genres2

--- plugins/english/wtrlab.ts
+++ plugins/english/wtrlab.ts
@@ -8,7 +8,7 @@ class WTRLAB implements Plugin.PluginBase {
   id = 'WTRLAB';
   name = 'WTR-LAB';
   site = 'https://wtr-lab.com/';
-  version = '1.1.6';
+  version = '1.1.7';
   icon = 'src/en/wtrlab/icon.png';
   sourceLang = 'en/';
   baggage = '';
