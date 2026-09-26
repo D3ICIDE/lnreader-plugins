0d3c9eb0b3876860509ca2d7cb472d514456abdc
D3ICIDE <183315953+D3ICIDE@users.noreply.github.com>
2026-09-26 11:56:39 +0530

    added header Cache-Control:no cache and cleanup

--- plugins/english/mvlempyr.ts
+++ plugins/english/mvlempyr.ts
@@ -30,7 +30,7 @@ class MVLEMPYRPlugin implements Plugin.PluginBase {
   name = 'MVLEMPYR';
   icon = 'src/en/mvlempyr/icon.png';
   site = 'https://www.mvlempyr.io/';
-  version = '1.0.19';
+  version = '1.0.15';
 
   _chapSite = 'https://chap.heliosarchive.online/';
   _allNovels: (Plugin.NovelItem & ExtraNovelData)[] | undefined;
